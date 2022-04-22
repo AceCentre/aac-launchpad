9a60450edba618a7153563371a80daa327d7cbb8
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-04-22 13:27:36 +0100

    Use shared types instead

--- packages/templates/src/index.ts
+++ packages/templates/src/index.ts
@@ -1,41 +1,4 @@
-type BoardID = string;
-type ButtonID = string;
-
-/**
- * Color attributes are string values and can be represented as rgb or rgba values.
- */
-type Color = string;
-
-type Button = {
-  id: ButtonID;
-  label: String;
-  border_color: Color;
-  background_color: Color;
-};
-
-type Grid = {
-  rows: number;
-  columns: number;
-  order: [[ButtonID]];
-};
-
-type Board = {
-  format: "open-board-0.1";
-  id: BoardID;
-  locale: "en-GB";
-  url?: string;
-  name: string;
-
-  /**
-   * The standard allows you to include html in this field, but this implementation
-   * only allows text and will render the html as plain text. This is to avoid having
-   * to worrying about parsing the html safely
-   */
-  description_html: string;
-
-  buttons: [Button];
-  grid: Grid;
-};
+import { Board } from "types";
 
 const simpleSingleTile: Board = {
   format: "open-board-0.1",
