2c66f48ebf8d3471f812d57b9ff400291d7f0760
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-04-22 16:18:28 +0100

    Add basic board-to-pdf

--- packages/board-to-pdf/src/index.ts
+++ packages/board-to-pdf/src/index.ts
@@ -1,5 +1,12 @@
 import { Board } from "types";
+import { jsPDF } from "jspdf";
 
-const boardToPdf = (board: Board, saveLocation: string) => {};
+const boardToPdf = (board: Board, saveLocation: string) => {
+  // Default export is a4 paper, portrait, using millimeters for units
+  const doc = new jsPDF();
+
+  doc.text("Hello world!", 10, 10);
+  doc.save(saveLocation);
+};
 
 export default boardToPdf;
