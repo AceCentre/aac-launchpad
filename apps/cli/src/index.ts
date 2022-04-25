8222e1159279de1d137fe698f7c50c00e0700804
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-04-25 12:49:31 +0100

    Output both PDFs

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 
 import boardToPdf from "board-to-pdf";
-import { simpleSingleTile } from "templates";
+import { simpleSingleTile, twoByTwo } from "templates";
 
 boardToPdf(simpleSingleTile, "./simple-single-tile.pdf");
+boardToPdf(twoByTwo, "./two-by-two.pdf");
