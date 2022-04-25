b94fc206d0ac7dcd9353a9f736fe1792af397c9b
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-04-25 12:48:52 +0100

    Add a grid

--- packages/templates/src/index.ts
+++ packages/templates/src/index.ts
@@ -6,6 +6,7 @@ export const simpleSingleTile: Board = {
   locale: "en-GB",
   name: "Simple Single Tile",
   description_html: "A board with a single tile in the centre",
+
   buttons: [
     {
       id: "main",
@@ -21,4 +22,47 @@ export const simpleSingleTile: Board = {
   },
 };
 
-export const ALL_TEMPLATES = [simpleSingleTile];
+export const twoByTwo: Board = {
+  format: "open-board-0.1",
+  id: "singleTile",
+  locale: "en-GB",
+  name: "Simple Single Tile",
+  description_html: "A board with a single tile in the centre",
+
+  buttons: [
+    {
+      id: "first",
+      label: "First",
+      border_color: "rgb(0, 0, 55)",
+      background_color: "rgba(200, 255, 255, 0.2)",
+    },
+    {
+      id: "second",
+      label: "Second",
+      border_color: "rgb(0, 0, 55)",
+      background_color: "rgba(200, 255, 255, 0.2)",
+    },
+    {
+      id: "third",
+      label: "Third",
+      border_color: "rgb(0, 0, 55)",
+      background_color: "rgba(200, 255, 255, 0.2)",
+    },
+    {
+      id: "fourth",
+      label: "Fourth",
+      border_color: "rgb(0, 0, 55)",
+      background_color: "rgba(200, 255, 255, 0.2)",
+    },
+  ],
+  grid: {
+    rows: 2,
+    columns: 2,
+    order: [
+      ["first", "second"],
+      ["third", "fourth"],
+    ],
+  },
+};
+
+export const ALL_TEMPLATES = [simpleSingleTile, twoByTwo];
