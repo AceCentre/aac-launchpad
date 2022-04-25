fa9fa5b1ee4a56fc57bda03a4685b1a66ba3713c
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-04-25 16:30:34 +0100

    Fill background colour properly

--- packages/board-to-pdf/src/index.ts
+++ packages/board-to-pdf/src/index.ts
@@ -6,6 +6,12 @@ type ButtonDimensions = {
   height: number;
 };
 
+type RBG = {
+  red: number;
+  green: number;
+  blue: number;
+};
+
 const WIDTH = 297;
 const HEIGHT = 210;
 const PADDING = 10;
@@ -34,6 +40,23 @@ const calculateButtonSize = (
   return { width: buttonWidth, height: buttonHeight };
 };
 
+const getRGB = (input: String): RBG => {
+  if (!input.startsWith("rgb(") || !input.endsWith(")")) {
+    throw new Error(
+      `Failed to parse '${input}' into an RGB color. Please specify your colors as RGB values. For example: rgb(255, 0, 0)`
+    );
+  }
+
+  const [red, green, blue] = input
+    .replace("rgb(", "")
+    .replace(")", "")
+    .split(",")
+    .map((x) => x.trim())
+    .map((x) => parseInt(x));
+
+  return { red, green, blue };
+};
+
 const boardToPdf = (board: Board, saveLocation: string) => {
   // Default export is a4 paper, portrait, using millimeters for units
   const doc = new jsPDF({
@@ -51,18 +74,36 @@ const boardToPdf = (board: Board, saveLocation: string) => {
 
   for (let rowCount = 0; rowCount < board.grid.rows; rowCount++) {
     for (let columnCount = 0; columnCount < board.grid.columns; columnCount++) {
+      const currentButtonId = board.grid.order[rowCount][columnCount];
+
+      const currentButton = board.buttons.find((x) => x.id === currentButtonId);
+
+      if (currentButton === undefined) {
+        throw new Error(
+          `Button referenced in Grid but not defined ('${currentButtonId}')`
+        );
+      }
+
+      const backgroundColor = getRGB(currentButton.background_color);
+
       const currentX = columnCount * (buttonDimensions.width + GAP) + PADDING;
       const currentY = rowCount * (buttonDimensions.height + GAP) + PADDING;
 
-      doc.roundedRect(
-        currentX,
-        currentY,
-        buttonDimensions.width,
-        buttonDimensions.height,
-        ROUNDED_BUTTONS,
-        ROUNDED_BUTTONS,
-        "F"
-      );
+      doc
+        .setFillColor(
+          backgroundColor.red,
+          backgroundColor.green,
+          backgroundColor.blue
+        )
+        .roundedRect(
+          currentX,
+          currentY,
+          buttonDimensions.width,
+          buttonDimensions.height,
+          ROUNDED_BUTTONS,
+          ROUNDED_BUTTONS,
+          "F"
+        );
     }
   }
 
