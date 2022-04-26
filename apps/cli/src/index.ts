777fad454e8348579fa37e1537dfdc70d7f8acc2
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-04-26 09:46:44 +0100

    Render new board

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,8 +1,14 @@
 #! /usr/bin/env node
 
 import boardToPdf from "board-to-pdf";
-import { simpleSingleTile, threeByThree, twoByTwo } from "templates";
+import {
+  alphabetChart,
+  simpleSingleTile,
+  threeByThree,
+  twoByTwo,
+} from "templates";
 
 boardToPdf(simpleSingleTile, "./test-files/simple-single-tile.pdf");
 boardToPdf(twoByTwo, "./test-files/two-by-two.pdf");
 boardToPdf(threeByThree, "./test-files/three-by-three.pdf");
+boardToPdf(alphabetChart, "./test-files/alphabet-chart.pdf");
