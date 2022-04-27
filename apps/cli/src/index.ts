dcf5bdc9ac750363ad2810822d75bcabf47c7373
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-04-27 16:02:23 +0100

    Update CLI

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 
 import boardToPdf from "board-to-pdf";
+import templateToBoard from "template-to-board";
 import {
   alphabetChart,
   simpleSingleTile,
@@ -8,7 +9,12 @@ import {
   twoByTwo,
 } from "templates";
 
-boardToPdf(simpleSingleTile, "./test-files/simple-single-tile.pdf");
-boardToPdf(twoByTwo, "./test-files/two-by-two.pdf");
-boardToPdf(threeByThree, "./test-files/three-by-three.pdf");
-boardToPdf(alphabetChart, "./test-files/alphabet-chart.pdf");
+const alphabetChartBoard = templateToBoard(alphabetChart, []);
+const simpleSingleTileBoard = templateToBoard(simpleSingleTile, []);
+const twoByTwoBoard = templateToBoard(twoByTwo, []);
+const threeByThreeBoard = templateToBoard(threeByThree, []);
+
+boardToPdf(simpleSingleTileBoard, "./test-files/simple-single-tile.pdf");
+boardToPdf(twoByTwoBoard, "./test-files/two-by-two.pdf");
+boardToPdf(threeByThreeBoard, "./test-files/three-by-three.pdf");
+boardToPdf(alphabetChartBoard, "./test-files/alphabet-chart.pdf");
