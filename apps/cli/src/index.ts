fd18c2cc019ff155bc1f7c05b8f2125280d143d1
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-04-28 09:39:23 +0100

    Switch to a proper cli

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -2,19 +2,138 @@
 
 import boardToPdf from "board-to-pdf";
 import templateToBoard from "template-to-board";
-import {
-  alphabetChart,
-  simpleSingleTile,
-  threeByThree,
-  twoByTwo,
-} from "templates";
+import path from "path";
+import { Result } from "types";
+import { ALL_TEMPLATES } from "templates";
 
-const alphabetChartBoard = templateToBoard(alphabetChart, []);
-const simpleSingleTileBoard = templateToBoard(simpleSingleTile, []);
-const twoByTwoBoard = templateToBoard(twoByTwo, []);
-const threeByThreeBoard = templateToBoard(threeByThree, []);
+import inquirer from "inquirer";
 
-boardToPdf(simpleSingleTileBoard, "./test-files/simple-single-tile.pdf");
-boardToPdf(twoByTwoBoard, "./test-files/two-by-two.pdf");
-boardToPdf(threeByThreeBoard, "./test-files/three-by-three.pdf");
-boardToPdf(alphabetChartBoard, "./test-files/alphabet-chart.pdf");
+const main = async () => {
+  const templateAnswer: { template: string } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "template",
+      message: "Choose a template",
+      choices: ALL_TEMPLATES.map((x) => {
+        return {
+          name: x.templateName,
+          value: x.id,
+          short: x.templateDescription,
+        };
+      }),
+    },
+  ]);
+
+  const chosenTemplate = ALL_TEMPLATES.find(
+    (x) => x.id === templateAnswer.template
+  );
+
+  if (chosenTemplate === undefined) {
+    throw new Error("You have chosen an invalid template");
+  }
+
+  const templateResults = await inquirer.prompt(
+    chosenTemplate.templateVariables.map((variable) => {
+      if (variable.type === "option") {
+        return {
+          type: "list",
+          name: variable.id,
+          message: variable.description,
+          choices: variable.options.map((option) => ({
+            name: option.label,
+            value: option.value,
+            short: option.description,
+          })),
+        };
+      }
+
+      if (variable.type === "freeText") {
+        return {
+          type: "input",
+          name: variable.id,
+          message: variable.description,
+          validate: (input) => {
+            if (input.length > variable.maxLength) {
+              return `Input must be less than ${variable.maxLength} characters`;
+            }
+            return true;
+          },
+        };
+      }
+
+      if (variable.type === "number") {
+        return {
+          type: "number",
+          name: variable.id,
+          message: variable.description,
+          validate: (input) => {
+            if (input > variable.max) {
+              return `Input must be lower than ${variable.max}`;
+            }
+            if (input < variable.min) {
+              return `Input must be higher than ${variable.min}`;
+            }
+            return true;
+          },
+        };
+      }
+      if (variable.type === "color") {
+        return {
+          type: "input",
+          name: variable.id,
+          message: variable.description,
+          validate: (input) => {
+            if (!input.startsWith("rgb(")) {
+              return `You must describe your color in rgb format. Example: rgb(255, 255, 255)`;
+            }
+            if (!input.endsWith(")")) {
+              return `You must describe your color in rgb format. Example: rgb(255, 255, 255)`;
+            }
+
+            const [red, green, blue] = input
+              .replace("rgb(", "")
+              .replace(")", "")
+              .split(",")
+              .map((x: string) => x.trim())
+              .map((x: string) => parseInt(x));
+
+            if (
+              red === undefined ||
+              green === undefined ||
+              blue === undefined
+            ) {
+              return `You must describe your color in rgb format. Example: rgb(255, 255, 255)`;
+            }
+
+            if (isNaN(red) || isNaN(green) || isNaN(blue)) {
+              return `You must describe your color in rgb format. Example: rgb(255, 255, 255)`;
+            }
+
+            return true;
+          },
+        };
+      }
+    })
+  );
+
+  const results: Array<Result> = Object.keys(templateResults).map((key) => {
+    return {
+      id: key,
+      value: templateResults[key],
+    };
+  });
+
+  const board = templateToBoard(chosenTemplate, results);
+
+  const pdfPath = path.join(`./${chosenTemplate.id}.pdf`);
+
+  boardToPdf(board, pdfPath);
+
+  console.log("");
+  console.log("✨✨✨✨✨ Successfully created a new board ✨✨✨✨✨");
+  console.log("");
+  console.log("Board saved to:", pdfPath);
+  console.log("");
+};
+
+main();
