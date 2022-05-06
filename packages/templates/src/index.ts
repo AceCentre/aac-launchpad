f9598cb24674f55da04607fdfc699eee232c41b3
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-05-06 10:48:16 +0100

    Add image board

--- packages/templates/src/index.ts
+++ packages/templates/src/index.ts
@@ -1,7 +1,12 @@
 import { alphabetChart } from "./alphabet-chart";
 import { simpleChoice } from "./simple-choice";
 import { Template } from "types";
+import { withImages } from "./with-images";
 
-export { alphabetChart, simpleChoice };
+export { alphabetChart, simpleChoice, withImages };
 
-export const ALL_TEMPLATES: Array<Template> = [alphabetChart, simpleChoice];
+export const ALL_TEMPLATES: Array<Template> = [
+  alphabetChart,
+  simpleChoice,
+  withImages,
+];
---
+++ packages/templates/src/with-images.ts
@@ -0,0 +1,52 @@
+import { Template } from "types";
+
+export const withImages: Template = {
+  templateId: "with-images",
+  templateName: "Two cell with images",
+  templateDescription: "Create a board that uses images",
+  templateImageUrl: "choice.png",
+  templateVariables: [],
+
+  format: "open-board-0.1",
+  id: "with-images",
+  name: "Create a board with images",
+  locale: "en-GB",
+  description_html: "A board that allows you to choose between two options",
+
+  ext_launchpad_options: {},
+
+  buttons: [
+    {
+      id: "left",
+      label: "Left",
+      border_color: "rgb(0,0,0)",
+      background_color: "rgb(255,255,255)",
+      ext_launchpad_label_color: "rgb(0,0,0)",
+      ext_launchpad_label_font_size: 30,
+      ext_launchpad_label_font_style: "bold",
+      image_id: "leftImage",
+    },
+    {
+      id: "right",
+      label: "Right",
+      border_color: "rgb(0,0,0)",
+      background_color: "rgb(255,255,255)",
+      ext_launchpad_label_color: "rgb(0,0,0)",
+      ext_launchpad_label_font_size: 30,
+      ext_launchpad_label_font_style: "bold",
+    },
+  ],
+
+  images: [
+    {
+      id: "leftImage",
+      url: "./left.png",
+    },
+  ],
+
+  grid: {
+    rows: 1,
+    columns: 2,
+    order: [["left", "right"]],
+  },
+};
