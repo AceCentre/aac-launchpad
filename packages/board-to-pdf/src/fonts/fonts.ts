351e7eef6150711795e1b43edd9b2b4392b35736
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-05-18 16:50:19 +0100

    Export option list

--- packages/board-to-pdf/src/fonts/fonts.ts
+++ packages/board-to-pdf/src/fonts/fonts.ts
@@ -2,7 +2,7 @@ import { initialise as initOpenDyslexicItalic } from "./OpenDyslexic-italic.js";
 import { initialise as initOpenDyslexicNormal } from "./OpenDyslexic-normal.js";
 import { initialise as initOpenDyslexicBold } from "./OpenDyslexic-bold.js";
 import { initialise as initOpenDyslexicBoldItalic } from "./OpenDyslexic-bolditalic.js";
-import { Font } from "types";
+import { Option } from "types";
 
 export const initialiseFonts = () => {
   initOpenDyslexicBold();
@@ -11,20 +11,16 @@ export const initialiseFonts = () => {
   initOpenDyslexicItalic();
 };
 
-export const VALID_FONTS: Array<Font> = [
+export const FONT_OPTIONS: Array<Option> = [
   {
-    name: "OpenDyslexic",
-    id: "OpenDyslexic",
-    styles: ["normal", "italic", "bolditalic", "bold"],
+    label: "OpenDyslexic",
+    value: "OpenDyslexic",
     description:
       "OpenDyslexic is a typeface designed against some common symptoms of dyslexia",
-    link: "https://opendyslexic.org/",
   },
   {
-    name: "Helvetica",
-    id: "helvetica",
-    styles: ["normal", "italic", "bolditalic", "bold"],
+    label: "Helvetica",
+    value: "helvetica",
     description: "Helvetica is a widely used sans-serif typeface",
-    link: "https://en.wikipedia.org/wiki/Helvetica",
   },
 ];
