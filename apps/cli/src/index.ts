13c414d65b2bfec5cd35a57b013b6e98c660959e
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2022-05-25 11:20:32 +0100

    Update the CLI to use presets

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -3,7 +3,7 @@
 import boardToPdf from "board-to-pdf";
 import templateToBoard from "template-to-board";
 import path from "path";
-import { Result } from "types";
+import { PresetVariable, PresetVariableValues, Result } from "types";
 import { ALL_TEMPLATES } from "templates";
 
 import inquirer from "inquirer";
@@ -33,104 +33,175 @@ const main = async () => {
     throw new Error("You have chosen an invalid template");
   }
 
-  const templateResults = await inquirer.prompt(
-    chosenTemplate.templateVariables.map((variable) => {
-      if (variable.type === "option") {
+  const presetResults = (await inquirer.prompt(
+    chosenTemplate.templateVariables
+      .filter((x) => x.type === "preset")
+      .map((variable) => {
+        const presetVariable = variable as PresetVariable;
         return {
           type: "list",
-          name: variable.id,
-          message: variable.description,
-          choices: variable.options.map((option) => ({
-            name: option.label,
-            value: option.value,
-            short: option.description,
+          name: presetVariable.id,
+          message: presetVariable.description,
+          choices: presetVariable.presets.map((preset) => ({
+            name: preset.label,
+            value: preset.variableValues,
+            short: preset.description,
           })),
         };
-      }
+      })
+  )) as { [key: string]: PresetVariableValues };
 
-      if (variable.type === "freeText") {
-        return {
-          type: "input",
-          name: variable.id,
-          message: variable.description,
-          validate: (input) => {
-            if (input.length > variable.maxLength) {
-              return `Input must be less than ${variable.maxLength} characters`;
-            }
-            return true;
-          },
-        };
-      }
+  let presetOverrides: { [key: string]: string } = {};
 
-      if (variable.type === "imageUrl") {
-        return {
-          type: "input",
-          name: variable.id,
-          message: variable.description,
-        };
-      }
+  for (const result of Object.values(presetResults).flat()) {
+    presetOverrides[result.id] = result.value;
+  }
 
-      if (variable.type === "number") {
-        return {
-          type: "number",
-          name: variable.id,
-          message: variable.description,
-          validate: (input) => {
-            if (input > variable.max) {
-              return `Input must be lower than ${variable.max}`;
-            }
-            if (input < variable.min) {
-              return `Input must be higher than ${variable.min}`;
-            }
-            return true;
-          },
-        };
-      }
-      if (variable.type === "color") {
-        return {
-          type: "input",
-          name: variable.id,
-          message: variable.description,
-          validate: (input) => {
-            if (!input.startsWith("rgb(")) {
-              return `You must describe your color in rgb format. Example: rgb(255, 255, 255)`;
-            }
-            if (!input.endsWith(")")) {
-              return `You must describe your color in rgb format. Example: rgb(255, 255, 255)`;
-            }
+  const templateResults = await inquirer.prompt(
+    chosenTemplate.templateVariables
+      .filter((x) => x.type !== "preset")
+      .filter((x) => x.hidden === false || x.hidden === undefined)
+      .sort((a, b) => {
+        if (a.type === "preset" && b.type !== "preset") {
+          return -1;
+        }
+        if (a.type !== "preset" && b.type === "preset") {
+          return 1;
+        }
+        return 0;
+      })
+      .map((variable) => {
+        if (variable.type === "option") {
+          return {
+            type: "list",
+            name: variable.id,
+            message: variable.description,
+            default: presetOverrides[variable.id],
+            choices: variable.options.map((option) => ({
+              name: option.label,
+              value: option.value,
+              short: option.description,
+            })),
+          };
+        }
 
-            const [red, green, blue] = input
-              .replace("rgb(", "")
-              .replace(")", "")
-              .split(",")
-              .map((x: string) => x.trim())
-              .map((x: string) => parseInt(x));
+        if (variable.type === "freeText") {
+          return {
+            type: "input",
+            name: variable.id,
+            message: variable.description,
+            default: presetOverrides[variable.id],
+            validate: (input) => {
+              if (input.length > variable.maxLength) {
+                return `Input must be less than ${variable.maxLength} characters`;
+              }
+              return true;
+            },
+          };
+        }
 
-            if (
-              red === undefined ||
-              green === undefined ||
-              blue === undefined
-            ) {
-              return `You must describe your color in rgb format. Example: rgb(255, 255, 255)`;
-            }
+        if (variable.type === "imageUrl") {
+          return {
+            type: "input",
+            name: variable.id,
+            message: variable.description,
+            default: presetOverrides[variable.id],
+          };
+        }
 
-            if (isNaN(red) || isNaN(green) || isNaN(blue)) {
-              return `You must describe your color in rgb format. Example: rgb(255, 255, 255)`;
-            }
+        if (variable.type === "number") {
+          return {
+            type: "number",
+            name: variable.id,
+            message: variable.description,
+            default: presetOverrides[variable.id],
+            validate: (input) => {
+              if (input > variable.max) {
+                return `Input must be lower than ${variable.max}`;
+              }
+              if (input < variable.min) {
+                return `Input must be higher than ${variable.min}`;
+              }
+              return true;
+            },
+          };
+        }
 
-            return true;
-          },
-        };
-      }
-    })
+        if (variable.type === "color") {
+          return {
+            type: "input",
+            name: variable.id,
+            message: variable.description,
+            default: presetOverrides[variable.id],
+            validate: (input) => {
+              if (!input.startsWith("rgb(")) {
+                return `You must describe your color in rgb format. Example: rgb(255, 255, 255)`;
+              }
+              if (!input.endsWith(")")) {
+                return `You must describe your color in rgb format. Example: rgb(255, 255, 255)`;
+              }
+
+              const [red, green, blue] = input
+                .replace("rgb(", "")
+                .replace(")", "")
+                .split(",")
+                .map((x: string) => x.trim())
+                .map((x: string) => parseInt(x));
+
+              if (
+                red === undefined ||
+                green === undefined ||
+                blue === undefined
+              ) {
+                return `You must describe your color in rgb format. Example: rgb(255, 255, 255)`;
+              }
+
+              if (isNaN(red) || isNaN(green) || isNaN(blue)) {
+                return `You must describe your color in rgb format. Example: rgb(255, 255, 255)`;
+              }
+
+              return true;
+            },
+          };
+        }
+
+        throw new Error(`You gave an invalid variable type: ${variable.type}`);
+      })
   );
 
-  const results: Array<Result> = Object.keys(templateResults).map((key) => {
-    return {
-      id: key,
-      value: templateResults[key],
-    };
-  });
+  const results: Array<Result> = chosenTemplate.templateVariables
+    .sort((a, b) => {
+      if (a.type === "preset" && b.type !== "preset") {
+        return 1;
+      }
+      if (a.type !== "preset" && b.type === "preset") {
+        return -1;
+      }
+      return 0;
+    })
+    .map((currentVariable) => {
+      // If the user gave an explicit answer use that
+      if (templateResults[currentVariable.id]) {
+        return {
+          id: currentVariable.id,
+          value: templateResults[currentVariable.id],
+        };
+      }
+
+      // If there was a preset answer give that
+      if (presetOverrides[currentVariable.id]) {
+        return {
+          id: currentVariable.id,
+          value: presetOverrides[currentVariable.id],
+        };
+      }
+
+      // Otherwise use the default value
+      return {
+        id: currentVariable.id,
+        value: currentVariable.defaultValue,
+      };
+    });
 
   const board = templateToBoard(chosenTemplate, results);
 
