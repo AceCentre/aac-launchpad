f2155639664bc253bb2edbb95c11712eda945790
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2023-01-27 15:54:04 +0000

    Add portrait test

--- packages/board-to-pdf/src/tests/screenshot.test.ts
+++ packages/board-to-pdf/src/tests/screenshot.test.ts
@@ -563,6 +563,44 @@ it("portrait", async () => {
   });
 });
 
+it("portrait-with-branding", async () => {
+  const rawBoard: Board = JSON.parse(
+    readFileSync(
+      path.join(__dirname, "./boards/portrait-with-branding.obf")
+    ).toString()
+  );
+
+  const { pdf } = await boardToPdf(rawBoard, {
+    rootToImages: path.join(__dirname, "./images"),
+    rootToPdfs: path.join(__dirname, "./pdfs"),
+  });
+
+  const output = Buffer.from(pdf);
+  const prep = fromBuffer(output, {
+    density: 500,
+    saveFilename: rawBoard.id,
+    savePath: path.join(__dirname, "./temp"),
+    format: "png",
+    height: 3508,
+    width: 2480,
+  });
+  if (prep.bulk === undefined) {
+    throw new Error(`Failed to get screenshot for: ${rawBoard.id}`);
+  }
+  await prep.bulk([1]);
+
+  const source = readFileSync(
+    path.join(__dirname, "./screenshots/portrait-with-branding.1.png")
+  );
+  const result = readFileSync(
+    path.join(__dirname, "./temp/portrait-with-branding.1.png")
+  );
+
+  expect(source).toMatchImage(result, {
+    diffPath: `./${rawBoard.id}-diff.png`,
+  });
+});
+
 it("core-with-branding", async () => {
   const rawBoard: Board = JSON.parse(
     readFileSync(
